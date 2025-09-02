4fb9c1532b560488be47ddfb832bdaf500c73c83
rahilshaikh20 <44061715+rahilshaikh20@users.noreply.github.com>
2025-09-02 23:24:40 +0530

    Update TestCase8.java

--- src/test/java/testCases/TestCase8.java
+++ src/test/java/testCases/TestCase8.java
@@ -6,11 +6,11 @@ import utilities.ExtentReportSetUp;
 
 public class TestCase8 extends ExtentReportSetUp {
       @Test
-    public void TC7() {
+    public void TC8() {
         test = extent.createTest("TC_0008");
         test.info("Verify user login using Page Factory");
           System.out.println("****TC 8 executed !!!!!!!!");
-          Assert.fail("Failed by Rahil !!!!");
+          //Assert.fail("Failed by Rahil !!!!");
 
     }
 
