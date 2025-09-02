35f9539af63952dd169fa018bbb6dcb8f8eb9bde
rahilshaikh20 <44061715+rahilshaikh20@users.noreply.github.com>
2025-09-03 00:53:11 +0530

    Update TestCase8.java

--- src/test/java/testCases/TestCase8.java
+++ src/test/java/testCases/TestCase8.java
@@ -1,16 +1,25 @@
 package testCases;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import utilities.ExtentReportSetUp;
 
 public class TestCase8 extends ExtentReportSetUp {
+    WebDriver driver;
       @Test
     public void TC8() {
         test = extent.createTest("TC_0008");
         test.info("Verify user login using Page Factory");
           System.out.println("****TC 8 executed !!!!!!!!");
-          //Assert.fail("Failed by Rahil !!!!");
+          WebDriverManager.chromedriver().setup();
+          driver = new ChromeDriver();
+          driver.get("https://playwright.dev/docs/intro");
+          driver.manage().window().maximize();
+          System.out.println("Title is: "+driver.getTitle());
+
 
     }
 
