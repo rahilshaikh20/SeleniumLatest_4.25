b16d543a146988a040b61d2fc5359bc35ffa2872
rahilshaikh20 <rahilshaikh595@yahoo.com>
2025-09-07 16:59:01 +0530

    Gradle 9.0 and Password encryptor added

--- src/main/java/utilities/DriverFactory.java
+++ src/main/java/utilities/DriverFactory.java
@@ -3,15 +3,15 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
 
+/****Refer Driver factory for a cleaner code instead of Base class***/
 public class DriverFactory {
 
     WebDriver driver;
@@ -24,15 +24,11 @@ public class DriverFactory {
         prop=DriverFactory.this.loadProperties();
         String browserName=  prop.getProperty("BrowserName");
 
-        //String browserName=  "Chrome";
-
-
         if (browserName.equals("Firefox")) {
             WebDriverManager.firefoxdriver().setup();
             driver = new FirefoxDriver();
 
         } else if (browserName.equalsIgnoreCase("Chrome")) {
-
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
 
@@ -53,11 +49,10 @@ public class DriverFactory {
         String url =prop.getProperty("URL");
         driver.get(url);
 
-        //driver.get("https://opensource-demo.orangehrmlive.com/");
     }
 
     public Properties loadProperties() throws IOException {
-        // prop = new Properties();
+
         fis = new FileInputStream(System.getProperty("user.dir") + "//Base.properties"); // For Mac use forward /
         System.out.println("From Load Properties method: "+fis);
         prop.load(fis);
