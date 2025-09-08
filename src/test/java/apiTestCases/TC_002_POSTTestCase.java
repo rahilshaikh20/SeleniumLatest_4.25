9b392d4a4478882883549c38dcb914adfe7f83af
rahilshaikh20 <rahilshaikh595@yahoo.com>
2025-09-08 22:48:55 +0530

    Added API testing with RestAssured

--- src/test/java/apiTestCases/TC_002_POSTTestCase.java
+++ src/test/java/apiTestCases/TC_002_POSTTestCase.java
@@ -2,32 +2,46 @@ package apiTestCases;
 import apiTesting.EndPoints;
 import apiTesting.PostData;
 import apiTesting.Routes;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class TC_002_POSTTestCase {
 
-   PostData postData;
+    PostData postData;
+    Response resp;
 
-   @BeforeTest
+    @BeforeTest
     public PostData setupPostData()
    {
        postData = new PostData();
 
-       postData.setName("Ashraf Shaikh");
+       postData.setName("Akshay Kumar");
        postData.setSalary(Routes.SALARY); //Getting value from constant
-       postData.setAge("64");
+       postData.setAge("59");
        return postData;
    }
 
    @Test
     public void testPOSTRequest()
    {
-       Response resp =EndPoints.methodPOST(postData);
+       resp =EndPoints.methodPOST(postData);
        resp.prettyPrint();
-       System.out.println("Status is: "+resp.statusCode());
+       System.out.println("Status code is: "+resp.statusCode());
 
+   }
+
+   @AfterTest
+    public void getResponseJSONdata()
+   {    //Get specific data from response by traversing the JSON path
+       JsonPath extractor = resp.jsonPath();
+
+       String name=extractor.get("data.name");
+       int ID= extractor.get("data.id");
+
+       System.out.println("Name retrived from Response: "+name);
+       System.out.println("ID retrived from Response: "+ ID);
    }
 }
