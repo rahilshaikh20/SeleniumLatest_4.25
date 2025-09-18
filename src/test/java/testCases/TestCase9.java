95707e6cda70bbf14bb370afc8cdd6c503330b4f
rahilshaikh20 <rahilshaikh595@yahoo.com>
2025-09-18 23:27:35 +0530

    added passed TCs

--- src/test/java/testCases/TestCase9.java
+++ src/test/java/testCases/TestCase9.java
@@ -1,4 +1,32 @@
 package testCases;
 
+import org.testng.annotations.Test;
+
 public class TestCase9 {
+
+    @Test
+    public void TC_9_1()
+    {
+        System.out.println(" *****  TC 9.1 PASSED !!!!");
+    }
+    @Test
+    public void TC_9_2()
+    {
+        System.out.println(" *****  TC 9.2 PASSED !!!!");
+    }
+    @Test
+    public void TC_9_3()
+    {
+        System.out.println(" *****  TC 9.3 PASSED !!!!");
+    }
+    @Test
+    public void TC_9_4()
+    {
+        System.out.println(" *****  TC 9.4 PASSED !!!!");
+    }
+    @Test
+    public void TC_9_5()
+    {
+        System.out.println(" *****  TC 9.5 PASSED !!!!");
+    }
 }
